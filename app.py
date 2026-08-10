3940864d2f7a3f4977b089c62511617e3d69a249
Rakshithahr344 <rakshithahr344@gmail.com>
2026-08-10 22:36:06 +0530

    README for Email-Writing-Assistants using RAG

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ except Exception as e:
 
 # Updated LLM configuration
 llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
+    model="gemini-1.5-flash",
     google_api_key=api_key,
     temperature=0.3
 )
